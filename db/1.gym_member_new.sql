7b3b84ec5091fcb31ec0871e5f85c3407bb8ec3d
iwansafr <iwansafr@gmail.com>
2021-03-15 19:08:50 +0700

    register

--- db/1.gym_member_new.sql
+++ db/1.gym_member_new.sql
@@ -11,6 +11,8 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `gym_member`;
 CREATE TABLE `gym_member` (
   `id` int NOT NULL,
+  `gym_id` int NOT NULL,
+  `user_id` int NOT NULL,
   `nama` varchar(255) NOT NULL,
   `alamat` text NOT NULL,
   `tgl_masuk` date NOT NULL,
@@ -18,16 +20,33 @@ CREATE TABLE `gym_member` (
   `hp` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=not active,1=active',
   `created` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `gym_member_paket`;
+CREATE TABLE `gym_member_paket` (
+  `id` int NOT NULL,
+  `gym_id` int NOT NULL,
+  `gym_member_id` int NOT NULL,
+  `gym_paket_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0=not active, 1= active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 ALTER TABLE `gym_member`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `gym_member_paket`
+  ADD PRIMARY KEY (`id`);
+
 
 ALTER TABLE `gym_member`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `gym_member_paket`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
